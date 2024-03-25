a5b11818bba82e406ac252500e0c174b4bf8dace
SSallaACR <ssalla@acr.org>
2024-03-25 14:23:40 -0400

    Initial commit

---
+++ Acr.Dart.FederatedJob/Services/FederatedJobService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Acr.Dart.FederatedJob.Services
+{
+    public class FederatedJobService : IFederatedJobService
+    {
+        private readonly string _connectionString;
+        public FederatedJobService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+        public void UpdateFederatedJobStatus(Guid transactionId, int status)
+        {
+            SqlConnection sqlConnection = null;
+            try
+            {
+                sqlConnection = new SqlConnection(_connectionString);
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand("GetFedJobForSiteByTransactionId", sqlConnection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                sqlCommand.Parameters.Add(new SqlParameter("@transactionId", transactionId));
+                sqlCommand.Parameters.Add(new SqlParameter("@status", status));
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch(Exception ex) {
+                throw ex;
+            }
+            finally {
+                sqlConnection?.Close();
+            }         
+        }
+
+        public bool CheckIfTransactionExists(Guid transactionId)
+        {
+
+            bool success = false;
+            SqlConnection sqlConnection = null;
+            try
+            {
+                sqlConnection = new SqlConnection(_connectionString);
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand("GetFedJobForSiteByTransactionId", sqlConnection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                sqlCommand.Parameters.Add(new SqlParameter("@transactionId", transactionId));
+                sqlCommand.ExecuteNonQuery(); 
+                success = true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;               
+            }
+            finally
+            {
+                if (success && sqlConnection.State == ConnectionState.Open) {
+                    sqlConnection?.Close();
+                }                
+            }
+            return success;
+        }
+    }
+}
---
+++ Acr.Dart.FederatedJob/Services/IFederatedJobService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acr.Dart.FederatedJob.Services
+{
+    public interface IFederatedJobService
+    {
+        void UpdateFederatedJobStatus(Guid transactionId, int status);
+        bool CheckIfTransactionExists(Guid transactionId);
+    }
+}
