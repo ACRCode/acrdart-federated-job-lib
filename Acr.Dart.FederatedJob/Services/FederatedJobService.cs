05cb77532e3efecca651c8b68ae6c920d2a2f76c
SSallaACR <ssalla@acr.org>
2024-04-05 09:33:53 -0400

    Resolved code review comments

--- Acr.Dart.FederatedJob/Services/FederatedJobService.cs
+++ Acr.Dart.FederatedJob/Services/FederatedJobService.cs
@@ -6,19 +6,17 @@ namespace Acr.Dart.FederatedJob.Services
 {
     public class FederatedJobService : IFederatedJobService
     {
-        private readonly string _connectionString;
-        public FederatedJobService(string connectionString)
+        private readonly SqlConnection _connectionString;       
+        public FederatedJobService(SqlConnection connectionString)
         {
             _connectionString = connectionString;
         }
         public bool UpdateFederatedJobStatus(Guid transactionId, int status)
-        {            
-            SqlConnection sqlConnection = null;
+        {                        
             try
             {
-                sqlConnection = new SqlConnection(_connectionString);
-                sqlConnection.Open();
-                SqlCommand sqlCommand = new SqlCommand("UpdateFedJobStatus", sqlConnection)
+                _connectionString.Open();
+                SqlCommand sqlCommand = new SqlCommand("UpdateFedJobStatus", _connectionString)
                 {
                     CommandType = CommandType.StoredProcedure
                 };
@@ -27,24 +25,17 @@ namespace Acr.Dart.FederatedJob.Services
                 var result = sqlCommand.ExecuteNonQuery();
                 return result > 0 ? true : false;
             }
-            catch(Exception ex) {                
-               
-                return false;
-                throw ex;
-            }
             finally {
-                sqlConnection?.Close();
+                _connectionString?.Close();
             }         
         }
 
         public bool CheckTransactionExists(Guid transactionId)
-        {           
-            SqlConnection sqlConnection = null;
+        {                      
             try
             {
-                sqlConnection = new SqlConnection(_connectionString);
-                sqlConnection.Open();
-                SqlCommand sqlCommand = new SqlCommand("GetFedJobForSite", sqlConnection)
+                _connectionString.Open();
+                SqlCommand sqlCommand = new SqlCommand("GetFedJobForSite", _connectionString)
                 {
                     CommandType = CommandType.StoredProcedure
                 };
@@ -59,26 +50,18 @@ namespace Acr.Dart.FederatedJob.Services
                     return false;
                 }
             }
-            catch (Exception ex)
-            {
-                throw ex;               
-            }
             finally
             {
-                if (sqlConnection.State == ConnectionState.Open) {
-                    sqlConnection?.Close();
-                }                
+                _connectionString?.Close();
             }          
         }
 
         public bool UpdateFederatedJobLogs(Guid transactionId, string logs)
-        {
-            SqlConnection sqlConnection = null;
+        {            
             try
             {
-                sqlConnection = new SqlConnection(_connectionString);
-                sqlConnection.Open();
-                SqlCommand sqlCommand = new SqlCommand("UpdateFedJobLogs", sqlConnection)
+                _connectionString.Open();
+                SqlCommand sqlCommand = new SqlCommand("UpdateFedJobLogs", _connectionString)
                 {
                     CommandType = CommandType.StoredProcedure
                 };
@@ -86,14 +69,10 @@ namespace Acr.Dart.FederatedJob.Services
                 sqlCommand.Parameters.Add(new SqlParameter("@Logs", logs));
                 var result = sqlCommand.ExecuteNonQuery();
                 return result > 0 ? true : false;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+            }            
             finally
             {
-                sqlConnection?.Close();
+                _connectionString?.Close();
             }
         }
     }
